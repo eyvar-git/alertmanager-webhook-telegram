74c0af627b6e9688aad97574d791f38652915113
eyvar-git <48128266+eyvar-git@users.noreply.github.com>
2019-05-27 15:21:21 +0300

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sed -i s/botToken/"$bottoken"/ flaskAlert.py
 sed -i s/xchatIDx/"$chatid"/ flaskAlert.py
+sed -i s/proxy_login/"$proxy_login"/ flaskAlert.py
+sed -i s/proxy_pass/"$proxy_pass"/ flaskAlert.py
+sed -i s/proxy_url/"$proxy_url"/ flaskAlert.py
+sed -i s/proxy_port/"$proxy_port"/ flaskAlert.py
 
 /usr/bin/gunicorn -w 4 -b 0.0.0.0:9119 flaskAlert:app
